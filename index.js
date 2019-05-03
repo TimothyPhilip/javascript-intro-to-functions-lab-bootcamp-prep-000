d8e4c2a032018becd42bbe521909bb3f2493d288
salubrious-reference-8531 <philippetimothe@gmail.com>
2019-05-03 03:26:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ if (string.toLowerCase() === string){
 return 'I can\'t hear you!'} else 
 if (string.toUpperCase() === string){
 return 'YES INDEED!'} else
-if {
+if (string){
 return 'I love you, Grandma.'}
 }
