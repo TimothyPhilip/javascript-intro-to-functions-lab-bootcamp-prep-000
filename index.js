3183374bec0d8f4a0241a79a1c296a7256b2b829
salubrious-reference-8531 <philippetimothe@gmail.com>
2019-05-03 03:14:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ console.log('hello')
 }
 
 function sayHiToGrandma(string)
-if (string.toLowerCase() === string)
+if (string.toLowerCase() === string){
 return 'I can\'t hear you!'
 }
 
-function sayHiToGrandma(string){
-if (string.toUpperCase() === string)
+function sayHiToGrandma(string)
+if (string.toUpperCase() === string){
 return 'YES INDEED'
 }
